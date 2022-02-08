0657fb6361072d7ebf972f26f6bdd5ed83013b42
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-02-08 10:38:48 +0800

    resolve errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
